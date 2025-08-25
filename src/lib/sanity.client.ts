378fece80c243f64af6ee2c8688ea323853c5ee6
I Made Bintang Astawa <astawabintang@gmail.com>
2025-08-25 20:00:56 +0800

    fix bug

--- src/lib/sanity.client.ts
+++ src/lib/sanity.client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+export const projectId = "mn07oouk";
+export const dataset = "production";
 const apiVersion = "2023-05-03";
 
 // Ambil token dari environment variable publik
